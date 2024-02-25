6cb73f960aefebe5cfc41094124c7fc2dd75016c
Romain Francois <romain@purrple.cat>
2024-02-25 13:38:06 +0100

    use bslib tooltips

--- R/auto.R
+++ R/auto.R
@@ -1,5 +1,41 @@
+#' @importFrom htmltools tags
 rx_valid_pkg <- "[a-zA-Z][a-zA-Z0-9.]*[a-zA-Z0-9]"
 rx_valid_fun <- "([a-zA-Z]\\w*|\\.\\w+)"
+
+get_title <- function(url) {
+  httr2::request(url) |>
+    httr2::req_perform() |>
+    httr2::resp_body_html() |>
+    xml2::xml_find_all("//title") |>
+    xml2::xml_text()
+}
+
+get_description <- function(url) {
+  httr2::request(url) |>
+    httr2::req_perform() |>
+    httr2::resp_body_html() |>
+    xml2::xml_find_all("//meta[@name='description']") |>
+    xml2::xml_attr("content")
+}
+
+tip_pkg <- function(pkg, title = get_title(url), ...) {
+  url <- downlit::href_package(pkg)
+  bslib::tooltip(
+    tags$a(glue::glue("{{{pkg}}}"), href = url, class = "r-link-pkg"),
+    title,
+    ...
+  )
+}
+
+tip_call <- function(call, title = get_title(url), ...) {
+  url <- downlit::autolink_url(call)
+
+  tip <- bslib::tooltip(
+    tags$a(call, href = url, class = "r-link-call"),
+    title,
+    ...
+  )
+}
 
 autolink_pkg <- function(x) {
   rx_pkg  <- glue::glue("[{{]({rx_valid_pkg})[}}]")
@@ -7,8 +43,7 @@ autolink_pkg <- function(x) {
     x, rx_pkg,
     function(pkg) {
       pkg <- stringr::str_extract(pkg, rx_pkg, group = 1)
-      url <- downlit::href_package(pkg)
-      glue::glue('<a href="{url}">{{{pkg}}}</a>')
+      as.character(tip_pkg(pkg))
     }
   )
 }
@@ -16,12 +51,17 @@ autolink_pkg <- function(x) {
 autolink_call <- function(x) {
   rx_call <- glue::glue("({rx_valid_pkg})::({rx_valid_fun})\\(\\)")
   stringr::str_replace_all(
-    x, rx_call, downlit::autolink
+    x, rx_call, function(call) {
+      as.character(tip_call(call))
+    }
   )
 
 }
 
 #' Setup automatic linking
+#'
+#' @examples
+#' auto()
 #'
 #' @export
 auto <- function() {
@@ -33,4 +73,8 @@ auto <- function() {
 
     default_text_hook(x)
   })
+
+  # This is a hack because I currently don't know how to inject the
+  # dependencies otherwise
+  bslib::tooltip("", "")
 }
